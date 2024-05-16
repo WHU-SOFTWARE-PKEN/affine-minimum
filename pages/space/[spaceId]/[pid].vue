1fefdb623b859ec447ac81ed593b0ac0e8c69bf3
crosmy <105200918+crosmy@users.noreply.github.com>
2024-05-16 14:36:34 +0800

    可以在页面中通过侧边栏访问不同的page了，添加了star和delete的icon

--- pages/space/[spaceId]/[pid].vue
+++ pages/space/[spaceId]/[pid].vue
@@ -2,13 +2,20 @@
 <script setup lang="ts">
 import { useRouteParams } from '@vueuse/router'
 import { ref, onMounted, watchEffect } from 'vue'
+import { assertExists } from '@blocksuite/store'
+import {
+    Document, Menu as IconMenu, CirclePlus, Setting, House, Delete, Plus, Star,StarFilled
+} from '@element-plus/icons-vue'
 const route = useRoute();
+const router = useRouter()
 // 定义 ref
 const editorContainer = ref<HTMLDivElement>()
-
 // 使用 useEditor 函数
-const editor = useEditor(route.params.spaceId, route.params.pid, 'page')
+const spaceId = route.params.spaceId as string;
+const pid = ref(route.params.pid);
+const editor = useEditor(spaceId, pid, 'page')
 
+const { workspace, pages } = useWorkspace(spaceId)
 // 等待编辑器初始化
 onMounted(async () => {
     await until(editor).toBeTruthy()
@@ -28,10 +35,48 @@ definePageMeta({
     name: 'WorkspacePage',
 })
 
+function goToNote(nspaceId: string, npid: string) {
+    router.push(`/space/${nspaceId}/${npid}`);
+}
+
+function handleAdd() {
+    assertExists(workspace.value)
+    assertExists(pages.value)
+
+    // eslint-disable-next-line no-alert
+    const id = prompt('Page ID')
+    if (!id) return
+
+    if (pages.value.some((p) => p.id === id)) {
+        // eslint-disable-next-line no-alert
+        alert('Page already exists')
+        return
+    }
+
+    const page = workspace.value.createPage({ id })
+    initPage(page)
+}
+
+async function handleDelete(id: string) {
+    assertExists(workspace.value)
+    assertExists(pages.value)
+
+    if (pid.value === id) {
+        pid.value = pages.value.find((p) => p.id !== id)!.id
+        await until(pid).not.toBe(id)
+    }
+
+    workspace.value.removePage(id)
+}
+const starfil = ref(true);
+async function handleStar() {
+  starfil.value = !starfil.value;
+}
+
 </script>
 
 <template>
-    <div class="common-layout">
+    <div>
         <el-container>
             <!-- 标题栏部分 -->
             <el-header>
@@ -40,9 +85,9 @@ definePageMeta({
                         <!-- 头像 -->
                         <div>
                             <el-avatar src="登录者的头像URL"></el-avatar>
-                        <span style="margin-left: 1px; position: relative;top: -10px; left:5px">登录者的昵称</span>
+                            <span style="margin-left: 1px; position: relative;top: -10px; left:5px">登录者的昵称</span>
                         </div>
-                        
+
                     </el-col>
                     <!-- 笔记软件的名称和图标 -->
                     <el-col :span="8" style="text-align: center; font-size: 30px;">
@@ -56,7 +101,7 @@ definePageMeta({
                             </el-menu-item>
                         </el-menu>
                     </el-col>
-                    
+
                 </el-row>
             </el-header>
 
@@ -71,11 +116,22 @@ definePageMeta({
                                         <message />
                                     </el-icon>我的笔记
                                 </template>
-                                <el-menu-item-group>
-                                    <el-menu-item index="1-1">Option 1</el-menu-item>
-                                    <el-menu-item index="1-2">Option 2</el-menu-item>
-                                    <el-menu-item index="1-3">Option 3</el-menu-item>
+                                <el-menu-item-group v-for="page in pages!" :key="page.id">
+                                        <!-- page.title是page页面自己填入的title部分，会同时反应在左边的目录上 -->
+                                        <el-menu-item @click="goToNote(spaceId, page.id)">
+                                            <el-icon v-if="starfil" @click ="handleStar" round>
+                                                <star />
+                                            </el-icon>
+                                            <el-icon v-else @click ="handleStar" round>
+                                                <StarFilled />
+                                            </el-icon>
+                                            {{ page.id }}
+                                            <el-icon v-if="pages!.length > 1" @click.stop="handleDelete(page.id)" round>
+                                                <delete />
+                                            </el-icon>
+                                        </el-menu-item>
                                 </el-menu-item-group>
+                                <el-menu-item @click="handleAdd"> 新建笔记</el-menu-item>
                             </el-sub-menu>
                             <el-sub-menu index="2">
                                 <template #title>
@@ -118,11 +174,8 @@ definePageMeta({
                 </el-aside>
 
                 <el-main>
-                    <div ref="editorContainer" class="editor-container"></div>
-                    <!-- <div>
-                        Space ID: {{ route.params.spaceId }}
-                        PID: {{ route.params.pid }}
-                    </div> -->
+                    <!-- <div ref="editorContainer"  style="text-align: left;"></div> -->
+                    <Editor :workspace-id="spaceId" :page-id="pid as string" />
                 </el-main>
             </el-container>
         </el-container>
