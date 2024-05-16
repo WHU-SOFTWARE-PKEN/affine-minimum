fb74d845a08b315ec8b54bd1b243ee36d32f851d
crosmy <105200918+crosmy@users.noreply.github.com>
2024-05-16 15:21:03 +0800

    添加了“我的设置”部分的按钮

--- pages/space/[spaceId]/[pid].vue
+++ pages/space/[spaceId]/[pid].vue
@@ -4,7 +4,8 @@ import { useRouteParams } from '@vueuse/router'
 import { ref, onMounted, watchEffect } from 'vue'
 import { assertExists } from '@blocksuite/store'
 import {
-    Document, Menu as IconMenu, CirclePlus, Setting, House, Delete, Plus, Star,StarFilled
+    Notebook, Menu as IconMenu, CirclePlus, Setting, House, Delete, Plus, Star,StarFilled,
+    ArrowLeft
 } from '@element-plus/icons-vue'
 const route = useRoute();
 const router = useRouter()
@@ -44,7 +45,7 @@ function handleAdd() {
     assertExists(pages.value)
 
     // eslint-disable-next-line no-alert
-    const id = prompt('Page ID')
+    const id = prompt('请为新建笔记命名：')
     if (!id) return
 
     if (pages.value.some((p) => p.id === id)) {
@@ -87,21 +88,18 @@ async function handleStar() {
                             <el-avatar src="登录者的头像URL"></el-avatar>
                             <span style="margin-left: 1px; position: relative;top: -10px; left:5px">登录者的昵称</span>
                         </div>
-
                     </el-col>
                     <!-- 笔记软件的名称和图标 -->
-                    <el-col :span="8" style="text-align: center; font-size: 30px;">
+                    <el-col :span="18" style="text-align: center; font-size: 30px;">
                         珞珈PK笔记
                     </el-col>
-                    <el-col :span="12">
+                    <el-col :span="2">
                         <el-menu mode="horizontal">
                             <el-menu-item index="1">
-                                <i class="el-icon-notebook-1"></i>
-                                <span slot="title">其他设置</span>
+                                <span slot="title">关于我们</span>
                             </el-menu-item>
                         </el-menu>
                     </el-col>
-
                 </el-row>
             </el-header>
 
@@ -110,30 +108,37 @@ async function handleStar() {
                 <el-aside width="200px">
                     <el-scrollbar>
                         <el-menu :default-openeds="['1', '3']">
-                            <el-sub-menu index="1">
+                            <el-menu-item index="1" @click="()=>{router.push(`/workspace`)}">
                                 <template #title>
                                     <el-icon>
-                                        <message />
+                                        <ArrowLeft />
+                                    </el-icon>退出当前工作区
+                                </template>
+                            </el-menu-item>
+                            <el-sub-menu index="2">
+                                <template #title>
+                                    <el-icon>
+                                        <Notebook />
                                     </el-icon>我的笔记
                                 </template>
                                 <el-menu-item-group v-for="page in pages!" :key="page.id">
-                                        <!-- page.title是page页面自己填入的title部分，会同时反应在左边的目录上 -->
-                                        <el-menu-item @click="goToNote(spaceId, page.id)">
-                                            <el-icon v-if="starfil" @click ="handleStar" round>
-                                                <star />
-                                            </el-icon>
-                                            <el-icon v-else @click ="handleStar" round>
-                                                <StarFilled />
-                                            </el-icon>
-                                            {{ page.id }}
-                                            <el-icon v-if="pages!.length > 1" @click.stop="handleDelete(page.id)" round>
-                                                <delete />
-                                            </el-icon>
-                                        </el-menu-item>
+                                    <!-- page.title是page页面自己填入的title部分，会同时反应在左边的目录上 -->
+                                    <el-menu-item @click="goToNote(spaceId, page.id)">
+                                        <el-icon v-if="starfil" @click="handleStar" round>
+                                            <star />
+                                        </el-icon>
+                                        <el-icon v-else @click="handleStar" round>
+                                            <StarFilled />
+                                        </el-icon>
+                                        {{ page.id }}
+                                        <el-icon v-if="pages!.length > 1" @click.stop="handleDelete(page.id)" round>
+                                            <delete />
+                                        </el-icon>
+                                    </el-menu-item>
                                 </el-menu-item-group>
                                 <el-menu-item @click="handleAdd"> 新建笔记</el-menu-item>
                             </el-sub-menu>
-                            <el-sub-menu index="2">
+                            <el-sub-menu index="3">
                                 <template #title>
                                     <el-icon><el-icon-star /></el-icon>我的收藏
                                 </template>
@@ -150,23 +155,69 @@ async function handleStar() {
                                     <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
                                 </el-sub-menu>
                             </el-sub-menu>
-                            <el-sub-menu index="3">
+                            <el-sub-menu index="4">
                                 <template #title>
                                     <el-icon>
                                         <setting />
                                     </el-icon>我的设置
                                 </template>
-                                <el-menu-item-group>
-                                    <template #title>Group 1</template>
-                                    <el-menu-item index="3-1">Option 1</el-menu-item>
-                                    <el-menu-item index="3-2">Option 2</el-menu-item>
-                                </el-menu-item-group>
-                                <el-menu-item-group title="Group 2">
-                                    <el-menu-item index="3-3">Option 3</el-menu-item>
-                                </el-menu-item-group>
-                                <el-sub-menu index="3-4">
-                                    <template #title>Option 4</template>
-                                    <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+                                <el-sub-menu index="4-1">
+                                    <template #title>账户设置</template>
+                                    <el-menu-item index="4-1-1">用户信息</el-menu-item>
+                                    <el-menu-item index="4-1-2">登录和密码</el-menu-item>
+                                    <el-menu-item index="4-1-3">账户同步</el-menu-item>
+                                </el-sub-menu>
+                                <el-sub-menu index="4-2">
+                                    <template #title>通用设置</template>
+                                    <el-menu-item index="4-2-1">语言</el-menu-item>
+                                    <el-menu-item index="4-2-2">主题</el-menu-item>
+                                    <el-menu-item index="4-2-3">字体</el-menu-item>
+                                    <el-menu-item index="4-2-4">导航</el-menu-item>
+                                </el-sub-menu>
+                                <el-sub-menu index="4-3">
+                                    <template #title>笔记设置</template>
+                                    <el-menu-item-group>
+                                        <el-menu-item index="4-3-1">默认笔记本</el-menu-item>
+                                        <el-menu-item index="4-3-2">自动保存</el-menu-item>
+                                        <el-menu-item index="4-3-3">格式设置</el-menu-item>
+                                        <el-menu-item index="4-3-4">模版管理</el-menu-item>
+                                    </el-menu-item-group>
+                                </el-sub-menu>
+                                <el-sub-menu index="4-4">
+                                    <template #title>隐私和安全</template>
+                                    <el-menu-item-group>
+                                        <el-menu-item index="4-4-1">数据加密</el-menu-item>
+                                        <el-menu-item index="4-4-2">两步验证</el-menu-item>
+                                        <el-menu-item index="4-4-3">隐私设置</el-menu-item>
+                                    </el-menu-item-group>
+                                </el-sub-menu>
+                                <el-sub-menu index="4-5">
+                                    <template #title>通知和提醒</template>
+                                    <el-menu-item-group>
+                                        <el-menu-item index="4-5-1">通知设置</el-menu-item>
+                                        <el-menu-item index="4-5-2">提醒和闹钟</el-menu-item>
+                                    </el-menu-item-group>
+                                </el-sub-menu>
+                                <el-sub-menu index="4-6">
+                                    <template #title>备份和同步</template>
+                                    <el-menu-item-group>
+                                        <el-menu-item index="4-6-1">备份笔记</el-menu-item>
+                                        <el-menu-item index="4-6-2">同步设置</el-menu-item>
+                                    </el-menu-item-group>
+                                </el-sub-menu>
+                                <el-sub-menu index="4-7">
+                                    <template #title>高级设置</template>
+                                    <el-menu-item-group>
+                                        <el-menu-item index="4-7-1">实验性功能</el-menu-item>
+                                        <el-menu-item index="4-7-2">自定义CSS</el-menu-item>
+                                        <el-menu-item index="4-7-3">API访问</el-menu-item>
+                                    </el-menu-item-group>
+                                </el-sub-menu>
+                                <el-sub-menu index="4-8">
+                                    <template #title>帮助和支持</template>
+                                    <el-menu-item index="4-22">用户手册</el-menu-item>
+                                    <el-menu-item index="4-23">联系我们</el-menu-item>
+                                    <el-menu-item index="4-24">关于软件</el-menu-item>
                                 </el-sub-menu>
                             </el-sub-menu>
                         </el-menu>
